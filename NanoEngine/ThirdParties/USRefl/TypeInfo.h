e5467d2d1e2d75e01039bbc2bce5f508a2a9c623
Shinonome <fishyu7471@outlook.com>
2020-11-25 22:53:59 +0800

    refl

---
+++ NanoEngine/ThirdParties/USRefl/TypeInfo.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace Ubpa::USRefl 
+{
+    // TypeInfoBase, attrs, fields
+    template<typename T>
+    struct TypeInfo;
+}
---
+++ NanoEngine/ThirdParties/USRefl/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef UBPA_USREFL_NOT_USE_NAMEOF
+  //#define UBPA_USREFL_USE_NAMEOF
+#endif
+
+// AutoRefl will use these names
+namespace UMeta 
+{
+    static constexpr char nameof_namespace[] = "UMeta";
+
+    static constexpr char initializer[] = "UMeta_initializer";
+    static constexpr char default_functions[] = "UMeta_default_functions";
+    static constexpr char constructor[] = "UMeta_constructor";
+    static constexpr char destructor[] = "UMeta_destructor";
+
+    static constexpr char nameof_initializer[] = "initializer";
+    static constexpr char nameof_default_functions[] = "default_functions";
+    static constexpr char nameof_constructor[] = "constructor";
+    static constexpr char nameof_destructor[] = "destructor";
+}
