0f967b78e67d3a9ee5d0aa5f098b959aeae436f9
Shinonome <fishyu7471@outlook.com>
2021-01-04 21:16:10 +0800

    operator new test

--- NanoEngine/Test/AllocatorTest.cpp
+++ NanoEngine/Test/AllocatorTest.cpp
@@ -2,12 +2,33 @@
 #include "NanoEngine/Common/Memory/Allocator.hpp"
 
 using namespace Nano;
-TEST(AllocatorTest, Test_Allocate) 
+
+namespace Test
 {
-    Allocator alloc = Allocator(4, 32, 4);
-    for (size_t i = 0; i < 20; ++i)
+    TEST(AllocatorTest, Test_Allocate) 
     {
-        void*p = alloc.Allocate();
-        alloc.Free(p);
+        Allocator alloc = Allocator(4, 32, 4);
+        for (size_t i = 0; i < 20; ++i)
+        {
+            void*p = alloc.Allocate();
+            alloc.Free(p);
+        }
+    }
+
+    class Test_Class
+    {
+        int32_t m_Index{ 0 };
+    public:
+        void* operator new(size_t size)
+        {
+            Allocator alloc = Allocator(4, 32, 4);
+            void* p = alloc.Allocate();
+            return p;
+        }
+    };
+
+    TEST(AllocatorTest, Test_OperatorNew)
+    {
+        Test_Class* alloc = new Test_Class();
     }
 }
