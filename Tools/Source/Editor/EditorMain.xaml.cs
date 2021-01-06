87392d6ff71ddf18196255e481b745c75827354f
Shinonome <fishyu7471@outlook.com>
2021-01-06 22:03:28 +0800

    editor

---
+++ Tools/Source/Editor/EditorMain.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using Panuon.UI.Silver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class EditorMain : WindowX
+    {
+        public EditorMain()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOpenItemClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = ".jpg|*.jpg|.png|*.png|.jpeg|*.jpeg";
+            if (dialog.ShowDialog(this) == false) 
+                return;
+            var fileName = dialog.FileName;
+        }
+    }
+}
